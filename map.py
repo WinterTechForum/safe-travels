a7e2db502b8653ed671f7d760a3484dfbdedb670
Joey Gibson <joey@joeygibson.com>
2025-03-05 12:03:48 -0700

    toolify mapping

--- map.py
+++ map.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-import json
 import os
 from typing import Any, List, Tuple
 
 import polyline
 import requests
+from langchain_core.tools import tool
+
 
 def get_lat_long(city_name: str) -> Tuple[float, float]:
     url = 'https://maps.googleapis.com/maps/api/geocode/json'
@@ -23,6 +24,7 @@ def get_lat_long(city_name: str) -> Tuple[float, float]:
     lng = data['results'][0]['geometry']['location']['lng']
 
     return lat, lng
+
 
 def compute_route(origin: Tuple[float, float], destination: Tuple[float, float]) -> dict[str, Any]:
     url = 'https://routes.googleapis.com/directions/v2:computeRoutes'
@@ -65,6 +67,7 @@ def compute_route(origin: Tuple[float, float], destination: Tuple[float, float])
 
     return response.json()
 
+
 def pick_equidistant_points(points: List[Tuple[float, float]], n: int = 10) -> List[Tuple[float, float]]:
     """Pick n equidistant points from a list of points."""
     if n <= 0:
@@ -73,23 +76,23 @@ def pick_equidistant_points(points: List[Tuple[float, float]], n: int = 10) -> L
 
     return [points[i] for i in range(0, len(points), step)]
 
+
+@tool
+def derive_route(city0: str, city1: str) -> List[Tuple[float, float]]:
+    """Derive a route between two cities."""
+    city0_coords = get_lat_long(city0)
+    city1_coords = get_lat_long(city1)
+
+    route = compute_route(city0_coords, city1_coords)
+
+    # decode the encodedPolyline from the response
+    encodedPolyline = route['routes'][0]['polyline']['encodedPolyline']
+
+    points = polyline.decode(encodedPolyline)
+
+    return pick_equidistant_points(points)
+
+
 if __name__ == '__main__':
-    try:
-        cb_coords = get_lat_long('Crested Butte, CO')
-        denver_coords = get_lat_long('Denver, CO')
-
-        route = compute_route(cb_coords, denver_coords)
-
-        # decode the encodedPolyline from the response
-        encodedPolyline = route['routes'][0]['polyline']['encodedPolyline']
-
-        points = polyline.decode(encodedPolyline)
-
-        print(points)
-        print(f'Number of points: {len(points)}')
-
-        equidistant_points = pick_equidistant_points(points)
-
-        print(equidistant_points)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    points = derive_route.run({'city0': 'Crested Butte, CO', 'city1': 'Denver, CO'})
+    print(points)
