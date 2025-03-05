cd70c7c7fb7685fee49593b500e70bc3df0969f2
Joey Gibson <joey@joeygibson.com>
2025-03-05 14:40:24 -0700

    calls assess_danger function

---
+++ danger_assessment.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from langchain_core.tools import tool
+
+
+@tool
+def assess_danger(point: dict[str, Any]) -> float:
+    """
+    Compute the danger score of a point based on the weather information.
+
+    Args:
+        point (dict[str, Any]): A point on a route to compute the danger score for.
+
+    Returns:
+        float: The danger score of the point.
+    """
+    print('Compute route danger score...')
+    print(point)
+
+    return 0.0
--- save_travels.py
+++ save_travels.py
@@ -7,6 +7,7 @@ from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
 
+from danger_assessment import assess_danger
 from routing import derive_route
 
 # Create the agent
@@ -14,35 +15,26 @@ memory = MemorySaver()
 model = ChatOpenAI(model="gpt-4o")
 search = TavilySearchResults(max_results=2)
 mapper = derive_route
+danger_accessor = assess_danger
 
-tools = [mapper, search]
+tools = [mapper, search, danger_accessor]
 agent_executor = create_react_agent(model, tools, checkpointer=memory)
 
 thread_id = uuid.uuid4().hex
 config = {"configurable": {"thread_id": thread_id}}
 
-for step in agent_executor.stream(
-        {"messages": [HumanMessage(content="derive a route from Crested Butte, CO to Denver, CO, departing at 7:00 AM on 2025-03-08.")]},
-        config,
-        stream_mode="values",
-):
-    step["messages"][-1].pretty_print()
+# HumanMessage(content="get the weather forecast at each point along that route, assuming we depart the origin city at 7:00 AM on 2025-03-08.")
 
-# for step in agent_executor.stream(
-#         {
-#             "messages": [
-#                 HumanMessage(content="get the weather forecast at each point along that route, assuming we depart the origin city at 7:00 AM on 2025-03-08.")
-#             ]
-#         },
-#         config,
-#         stream_mode="values",
-# ):
-#     step["messages"][-1].pretty_print()
 
 for step in agent_executor.stream(
         {
             "messages": [
-                HumanMessage(content="get any whether events at each point along that route, assuming we depart the origin city at 7:00 AM on 2025-03-08.")
+                HumanMessage(content='derive a route from Crested Butte, CO to Denver, CO, '
+                                     'departing at 7:00 AM on 2025-03-08.'),
+                HumanMessage(content='get any weather events at each point along that route, '
+                                     'assuming we depart the origin city at 7:00 AM on 2025-03-08.'),
+                HumanMessage(content='asses the danger of each point along that route using '
+                                     'the results from the previous step.')
             ]
         },
         config,
