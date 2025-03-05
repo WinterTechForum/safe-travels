8415be3f29b8a9e8b057196ab82e28c8af3ba5af
Joey Gibson <joey@joeygibson.com>
2025-03-05 11:42:20 -0700

    refactored to be prettier

--- map.py
+++ map.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import json
 import os
+from typing import Any
 
 import polyline
 import requests
 
-# write a function to take a city name, and make a call to the google maps API to get the latitude and longitude
 def get_lat_long(city_name):
     url = 'https://maps.googleapis.com/maps/api/geocode/json'
     params = {
@@ -20,58 +20,67 @@ def get_lat_long(city_name):
 
     return (lat, lng)
 
-cb_coords = get_lat_long('Crested Butte, CO')
-denver_coords = get_lat_long('Denver, CO')
-
-url = 'https://routes.googleapis.com/directions/v2:computeRoutes'
-headers = {
-    'Content-Type': 'application/json',
-    'X-Goog-Api-Key': os.environ['GOOGLE_MAPS_API_KEY'],
-    'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
-}
-data = {
-    "origin": {
-        "location": {
-            "latLng": {
-                "latitude": cb_coords[0],
-                "longitude": cb_coords[1]
+def compute_route(origin: tuple[int, int], destination: tuple[int, int]) -> dict[str, Any]:
+    url = 'https://routes.googleapis.com/directions/v2:computeRoutes'
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': os.environ['GOOGLE_MAPS_API_KEY'],
+        'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+    }
+    data = {
+        "origin": {
+            "location": {
+                "latLng": {
+                    "latitude": origin[0],
+                    "longitude": origin[1]
+                }
             }
-        }
-    },
-    "destination": {
-        "location": {
-            "latLng": {
-                "latitude": denver_coords[0],
-                "longitude": denver_coords[1]
+        },
+        "destination": {
+            "location": {
+                "latLng": {
+                    "latitude": destination[0],
+                    "longitude": destination[1]
+                }
             }
-        }
-    },
-    "travelMode": "DRIVE",
-    "routingPreference": "TRAFFIC_AWARE",
-    "computeAlternativeRoutes": False,
-    "routeModifiers": {
-        "avoidTolls": False,
-        "avoidHighways": False,
-        "avoidFerries": False
-    },
-    "languageCode": "en-US",
-    "units": "IMPERIAL"
-}
+        },
+        "travelMode": "DRIVE",
+        "routingPreference": "TRAFFIC_AWARE",
+        "computeAlternativeRoutes": False,
+        "routeModifiers": {
+            "avoidTolls": False,
+            "avoidHighways": False,
+            "avoidFerries": False
+        },
+        "languageCode": "en-US",
+        "units": "IMPERIAL"
+    }
 
-response = requests.post(url, headers=headers, json=data)
-print(response.json())
+    response = requests.post(url, headers=headers, json=data)
 
-# decode the encodedPolyline from the response
-encodedPolyline = response.json()['routes'][0]['polyline']['encodedPolyline']
+    return response.json()
 
-points = polyline.decode(encodedPolyline)
 
-print(points)
-print(f'Number of points: {len(points)}')
+def pick_equidistant_points(n: int = 10) -> list[tuple[int, int]]:
+    step = len(points) // n
 
-# extract 10 equidistant points from the list of points
-n = 10
-step = len(points) // n
-equidistant_points = [points[i] for i in range(0, len(points), step)]
+    return [points[i] for i in range(0, len(points), step)]
 
-print(equidistant_points)
+
+if __name__ == '__main__':
+    cb_coords = get_lat_long('Crested Butte, CO')
+    denver_coords = get_lat_long('Denver, CO')
+
+    route = compute_route(cb_coords, denver_coords)
+
+    # decode the encodedPolyline from the response
+    encodedPolyline = route['routes'][0]['polyline']['encodedPolyline']
+
+    points = polyline.decode(encodedPolyline)
+
+    print(points)
+    print(f'Number of points: {len(points)}')
+
+    points = pick_equidistant_points()
+
+    print(points)
